fea63b5e8c658536832e03427169c032f5c7e9d4
Hubertius <hubert.kopec@interia.pl>
2023-06-08 14:35:16 +0200

    Creation of first service

---
+++ src/main/java/guru/springframework/spring6webapp/services/BookService.java
@@ -0,0 +1,20 @@
+package guru.springframework.spring6webapp.services;
+
+import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.repositories.BookRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService implements IBookService{
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Iterable<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
